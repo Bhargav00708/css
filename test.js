a0a4771411fff5a9295399660a1fcc901d8d1509
Bhargav <vanrabhargav@gmail.com>
2020-05-13 16:20:26 +0530

    hii

---
+++ test.js
@@ -0,0 +1,5 @@
+<a href = "http://www.umich.edu" onclick = "return true">University of Michigan</a><br/>
+
+<a href = "#" onclick = "alert(this.href)">University of Michigan </a><br/>	
+
+<a href = "page2.html" onclick = "alert(this.href)">University of Michigan </a><br/>
